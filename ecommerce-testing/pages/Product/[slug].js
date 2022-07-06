413c91f063010e203fa1e746d61a2f15c725a4c1
Maadeyo <Maadeyo@gmail.com>
2022-07-06 09:36:33 +0300

    product page logic

---
+++ ecommerce-testing/pages/Product/[slug].js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { client, urlFor } from '../../library/client'; 
+
+const ProductDetails = ({product, products}) => { const { image, name, details, price, } = product;
+  return (
+    <div>
+        <div className='product-detail-container'>
+            <div>
+                <div className='image-container'>
+                    <img src={urlFor(image && image[0])}/>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export const getStaticProps = async ({ params: { slug }}) => {
+    const query = `*[_type == "product" && slug.current == '${slug}][0]`;
+    const productsQuery = '*[_type == "product"]';
+    const product = await client.fetch(query);
+    const products = await client.fetch(productsQuery);
+  
+    const bannerQuery = '*[_type == "banner"]';
+    const bannerData = await client.fetch(bannerQuery);
+  
+    return {
+      props: { products, product }
+    }
+  }
+
+export default ProductDetails
